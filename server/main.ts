b06466d7063e5acb1a919454954bf19e199a6345
client-side96 <dev@clientside.tech>
2024-07-23 20:32:12 +0200

    refactor: use own express server instead remix-serve

---
+++ server/main.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { createRequestHandler } from "@remix-run/express";
+
+import * as build from "../build/server/index.js";
+
+const app = express();
+
+app.use(express.static("build/client"));
+
+app.all("*", createRequestHandler({ build }));
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
