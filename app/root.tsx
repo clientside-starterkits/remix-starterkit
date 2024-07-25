88482ab1beb6ce2e05da245026dace06a5246cb3
client-side96 <dev@clientside.tech>
2024-07-25 22:04:04 +0200

    chore: add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,10 @@
+import { LinksFunction } from '@remix-run/node';
 import { Links, Meta, Outlet, Scripts } from '@remix-run/react';
+import stylesheet from './tailwind.css?url';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: stylesheet },
+];
 
 export default function App() {
   return (
@@ -8,7 +14,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <h1>Hello world!</h1>
+        <h1 className="text-3xl text-red-900">Hello world!</h1>
         <Outlet />
         <Scripts />
       </body>
