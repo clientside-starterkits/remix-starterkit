bf6ba816d13a31f14f41e3a7505bfefb0a4d6a3b
client-side96 <dev@clientside.tech>
2024-10-12 14:08:54 +0200

    style: fix biome warnings and errors

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,10 +1,10 @@
-import { PassThrough } from 'node:stream';
+import { PassThrough } from "node:stream";
 
-import type { AppLoadContext, EntryContext } from '@remix-run/node';
-import { createReadableStreamFromReadable } from '@remix-run/node';
-import { RemixServer } from '@remix-run/react';
-import * as isbotModule from 'isbot';
-import { renderToPipeableStream } from 'react-dom/server';
+import type { AppLoadContext, EntryContext } from "@remix-run/node";
+import { createReadableStreamFromReadable } from "@remix-run/node";
+import { RemixServer } from "@remix-run/react";
+import * as isbotModule from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
 
 const ABORT_DELAY = 5_000;
 
@@ -16,7 +16,7 @@ export default function handleRequest(
   _loadContext: AppLoadContext,
 ) {
   const _prohibitOutOfOrderStreaming =
-    isBotRequest(request.headers.get('user-agent')) || remixContext.isSpaMode;
+    isBotRequest(request.headers.get("user-agent")) || remixContext.isSpaMode;
 
   return _prohibitOutOfOrderStreaming
     ? handleBotRequest(
@@ -42,12 +42,12 @@ function isBotRequest(userAgent: string | null) {
   }
 
   // isbot >= 3.8.0, >4
-  if ('isbot' in isbotModule && typeof isbotModule.isbot === 'function') {
+  if ("isbot" in isbotModule && typeof isbotModule.isbot === "function") {
     return isbotModule.isbot(userAgent);
   }
 
   // isbot < 3.8.0
-  if ('default' in isbotModule && typeof isbotModule.default === 'function') {
+  if ("default" in isbotModule && typeof isbotModule.default === "function") {
     return isbotModule.default(userAgent);
   }
 
@@ -60,6 +60,7 @@ function handleBotRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
+  let statusCode = responseStatusCode;
   return new Promise((resolve, reject) => {
     let shellRendered = false;
     const { pipe, abort } = renderToPipeableStream(
@@ -74,7 +75,7 @@ function handleBotRequest(
           const body = new PassThrough();
           const stream = createReadableStreamFromReadable(body);
 
-          responseHeaders.set('Content-Type', 'text/html');
+          responseHeaders.set("Content-Type", "text/html");
 
           resolve(
             new Response(stream, {
@@ -89,7 +90,7 @@ function handleBotRequest(
           reject(error);
         },
         onError(error: unknown) {
-          responseStatusCode = 500;
+          statusCode = 500;
           // Log streaming rendering errors from inside the shell.  Don't log
           // errors encountered during initial shell rendering since they'll
           // reject and get logged in handleDocumentRequest.
@@ -110,6 +111,7 @@ function handleBrowserRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
+  let statusCode = responseStatusCode;
   return new Promise((resolve, reject) => {
     let shellRendered = false;
     const { pipe, abort } = renderToPipeableStream(
@@ -124,7 +126,7 @@ function handleBrowserRequest(
           const body = new PassThrough();
           const stream = createReadableStreamFromReadable(body);
 
-          responseHeaders.set('Content-Type', 'text/html');
+          responseHeaders.set("Content-Type", "text/html");
 
           resolve(
             new Response(stream, {
@@ -139,7 +141,7 @@ function handleBrowserRequest(
           reject(error);
         },
         onError(error: unknown) {
-          responseStatusCode = 500;
+          statusCode = 500;
           // Log streaming rendering errors from inside the shell.  Don't log
           // errors encountered during initial shell rendering since they'll
           // reject and get logged in handleDocumentRequest.
--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,14 @@
-import { LinksFunction } from '@remix-run/node';
-import { Links, Meta, Outlet, Scripts } from '@remix-run/react';
-import stylesheet from './tailwind.css?url';
+import type { LinksFunction } from "@remix-run/node";
+import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import stylesheet from "./tailwind.css?url";
 
 export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: stylesheet },
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export default function App() {
   return (
-    <html>
+    <html lang="en">
       <head>
         <Meta />
         <Links />
